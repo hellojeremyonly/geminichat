b518b6276076fcd142eb28fc9cf769dfc7e43fd3
Jeremy <128933625+hellojeremyonly@users.noreply.github.com>
2023-12-24 22:08:10 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -14,7 +14,7 @@
 
 import inspect
 import textwrap
-
+import requests
 import streamlit as st
 
 
@@ -26,3 +26,35 @@ def show_code(demo):
         st.markdown("## Code")
         sourcelines, _ = inspect.getsourcelines(demo)
         st.code(textwrap.dedent("".join(sourcelines[1:])))
+        
+def call_gemini_pro_api(prompt):
+    """
+    This function makes a POST request to the Gemini Pro API
+    to generate content based on the given prompt.
+    """
+    # Replace these with your actual project and region values
+    region = "your-region"
+    project_id = "your-project-id"
+    
+    url = f"https://{region}-aiplatform.googleapis.com/v1/projects/{project_id}/locations/{region}/publishers/google/models/gemini-pro:streamGenerateContent"
+    
+    # Prepare headers and payload as required by the API
+    headers = {
+        'Authorization': 'Bearer YOUR_ACCESS_TOKEN',  # Ensure you have a valid token
+        'Content-Type': 'application/json',
+    }
+
+    data = {
+        'prompt': prompt,
+        # Add other parameters as required by the API
+    }
+    
+    response = requests.post(url, headers=headers, json=data)
+    
+    # Handle the response
+    if response.status_code == 200:
+        # Parse the response content and return the text
+        return response.json()
+    else:
+        # Handle errors (you could raise an exception or return an error message)
+        return f"Error: {response.status_code} - {response.text}"
